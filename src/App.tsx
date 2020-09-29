ef3bcb36be34f97491d55e7fe7c09a1527b7236f
Jessica Rocha <jessicarocha.anjos@gmail.com>
2020-09-28 22:53:52 -0300

    Moving page decision to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
+import General from './pages/General';
+import Monthly from './pages/Monthly';
 
 import MenuBar from './components/MenuBar';
-import Content from './components/Content';
+
+const Content = (props: { page: string }) => {
+  if (props.page === 'Mensal') return <Monthly />
+  if (props.page === 'Pay Day') return <></>
+  return <General />
+}
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Geral');
-  
+
   return <>
-    <MenuBar activeItem={currentPage} onClick={setCurrentPage}/>
-    <Content currentPage={currentPage}/>
+    <MenuBar activeItem={currentPage} onClick={setCurrentPage} />
+    <Content page={currentPage} />
   </>;
 }
 
